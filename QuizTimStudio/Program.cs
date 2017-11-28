fe077988cdfa977c29fd5456518ec93c3effac30
Blake <welzie@gmail.com>
2017-11-27 17:33:53 -0800

    Add project files.

---
+++ QuizTimStudio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuizTimStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
