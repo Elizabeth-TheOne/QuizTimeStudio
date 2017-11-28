6ac22bf6f7cb3e63d8dfe884b00fc1256dd688d0
Blake <welzie@gmail.com>
2017-11-27 18:33:15 -0800

    my first attempt at solving this, does not include checkbox

--- QuizTimStudio/Program.cs
+++ QuizTimStudio/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace QuizTimStudio
+namespace QuizTimeStudio
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var birdAnswers = new List<string>() {"Code", "Bake", "Fly"};
+            var doBirds = new MultipleChoiceQuestion("Do Birds?", birdAnswers, "Fly");
+
+            var catAnswers = new List<string>() { "Sleep", "Go to mars", "Mow the grass" };
+            var doCats = new MultipleChoiceQuestion("Do Cats?", catAnswers, "Sleep");
+
+            var trueOrFalse = new TrueFalseQuestion("Is the sky blue?", "True");
+
+            var questions = new List<Question>() { doBirds, doCats, trueOrFalse };
+            foreach(Question question in questions)
+            {
+                question.Display();
+                Console.WriteLine("Correct? " + question.IsCorrect());
+            }
+
         }
     }
 }
